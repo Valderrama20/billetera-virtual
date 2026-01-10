07939b8dc3373b06fd3357d0780f221277773996
valderrama20 <jose23122009@gmail.com>
2026-01-10 14:04:09 +0100

    primer commit

---
+++ src/main/java/billeteraVirtual/BilleteraVirtualApplication.java
@@ -0,0 +1,13 @@
+package billeteraVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteraVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleteraVirtualApplication.class, args);
+	}
+
+}
