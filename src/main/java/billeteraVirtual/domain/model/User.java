601c0ac4bb35dea7716e828ceadefc8dbfd5a14c
valderrama20 <jose23122009@gmail.com>
2026-01-10 15:57:18 +0100

    feat: agregar clase de usuario y el enum rol de usuario

---
+++ src/main/java/billeteraVirtual/domain/model/User.java
@@ -0,0 +1,117 @@
+package billeteraVirtual.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class User {
+
+    // Atributos
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserRole role;
+    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+    // Constructor
+
+    // 1. CONSTRUCTOR VACÍO (Esencial)
+    // Los frameworks (Hibernate, Jackson, etc.) usan este constructor
+    // para crear la instancia vacía y luego llenan los datos usando los Setters.
+    public User() {
+    }
+
+    // 2. CONSTRUCTOR PARA "NUEVOS" USUARIOS (Business Logic)
+    // Se usa cuando el usuario se registra en la API.
+    // Aquí SÍ generamos el ID y la fecha, porque es algo nuevo.
+    public User(String firstName, String lastName, String email, String password) {
+        this.id = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = UserRole.USER;
+        this.updatedAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // 3. CONSTRUCTOR PARA "RECONSTRUIR" (Desde DB manualmente)
+    // Si usas un mapper manual (como RowMapper en JDBC), usarás este.
+    // Aquí NO generamos nada, solo asignamos lo que viene de la DB.
+    public User(UUID id, String firstName, String lastName, String email, String password, UserRole role, LocalDateTime updatedAt, LocalDateTime createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
+    }
+
+    // Getters and Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/billeteraVirtual/domain/model/UserRole.java
@@ -0,0 +1,6 @@
+package billeteraVirtual.domain.model;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
